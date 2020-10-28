884b905bd8615b7c4150464dee7b2a65d1074ac2
Tal Almagor <almagoric@gmail.com>
2020-10-28 13:24:32 +0200

    Fix module level imports.

--- spacy_ke/__init__.py
+++ spacy_ke/__init__.py
@@ -1,3 +1,5 @@
 from spacy_ke.about import *
 from spacy_ke.yake import Yake
 from spacy_ke.textrank import TextRank
+from spacy_ke.positionrank import PositionRank
+from spacy_ke.topicrank import TopicRank
